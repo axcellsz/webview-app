0a665716198c54220caf1b7b82c292d37092a458
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-12-29 23:29:36 +0700

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,6 @@
+// Jika file HTML berada di domain worker yang sama, biarkan API_BASE = ""
+// Jika beda domain, isi: "https://xxxx.workers.dev"
+window.APP_CONFIG = {
+  API_BASE: "",
+  SYNC_KEY: "kunci_sinkron_123"
+};
